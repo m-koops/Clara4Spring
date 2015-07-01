a0ac7c45e751335240d26afd63448479cea21aff
Mark Koops <mkoops@bol.com>
2015-07-01 17:54:16 +0200

    let attribute filters be discovered from the Spring container

--- src/main/java/org/vaadin/clara/spring/SpringClara.java
+++ src/main/java/org/vaadin/clara/spring/SpringClara.java
@@ -17,6 +17,7 @@ package org.vaadin.clara.spring;
 
 import java.io.InputStream;
 import java.util.Collections;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -29,8 +30,8 @@ import org.vaadin.teemu.clara.inflater.filter.AttributeFilter;
 import com.vaadin.ui.Component;
 
 /**
- * Inspired by {@link Clara}, with the extension that this Class is also able to resolve (autowire)
- * components from a Spring application context.
+ * Inspired by {@link Clara}, with the extension that this Class is also able to
+ * resolve (autowire) components from a Spring application context.
  */
 @org.springframework.stereotype.Component
 public class SpringClara {
@@ -42,12 +43,13 @@ public class SpringClara {
 	}
 
 	/**
-	 * Returns a {@link Component} that is read from the XML representation given as
-	 * {@link InputStream}. If you would like to bind the resulting {@link Component} to a
-	 * controller object, you should use {@link #create(InputStream, Object, AttributeFilter...)}
-	 * method instead.
+	 * Returns a {@link Component} that is read from the XML representation
+	 * given as {@link InputStream}. If you would like to bind the resulting
+	 * {@link Component} to a controller object, you should use
+	 * {@link #create(InputStream, Object, AttributeFilter...)} method instead.
 	 * 
-	 * @param xml XML representation.
+	 * @param xml
+	 *            XML representation.
 	 * @return a {@link Component} that is read from the XML representation.
 	 */
 	public Component create(InputStream xml) {
@@ -55,61 +57,77 @@ public class SpringClara {
 	}
 
 	/**
-	 * Returns a {@link Component} that is read from an XML file in the classpath and binds the
-	 * resulting {@link Component} to the given {@code controller} object.
+	 * Returns a {@link Component} that is read from an XML file in the
+	 * classpath and binds the resulting {@link Component} to the given
+	 * {@code controller} object.
 	 * 
 	 * <br>
 	 * <br>
-	 * The filename is given either as a path relative to the class of the {@code controller} object
-	 * or as an absolute path. For example if you have a {@code MyController.java} and
-	 * {@code MyController.xml} files in the same package, you can call this method like
+	 * The filename is given either as a path relative to the class of the
+	 * {@code controller} object or as an absolute path. For example if you have
+	 * a {@code MyController.java} and {@code MyController.xml} files in the
+	 * same package, you can call this method like
 	 * {@code Clara.create("MyController.xml", new MyController())}.
 	 * 
 	 * <br>
 	 * <br>
-	 * Optionally you may also provide {@link AttributeFilter}s to do some modifications (or example
-	 * localized translations) to any attributes present in the XML representation.
+	 * Optionally you may also provide {@link AttributeFilter}s to do some
+	 * modifications (or example localized translations) to any attributes
+	 * present in the XML representation.
 	 * 
-	 * @param xmlClassResourceFileName filename of the XML representation (within classpath,
-	 *            relative to {@code controller}'s class or absolute path).
-	 * @param controller controller object to bind the resulting {@code Component} (non-{@code null}
-	 *            ).
-	 * @param attributeFilters optional {@link AttributeFilter}s to do attribute modifications.
-	 * @return a {@link Component} that is read from the XML representation and bound to the given
-	 *         {@code controller}.
+	 * @param xmlClassResourceFileName
+	 *            filename of the XML representation (within classpath, relative
+	 *            to {@code controller}'s class or absolute path).
+	 * @param controller
+	 *            controller object to bind the resulting {@code Component}
+	 *            (non-{@code null} ).
+	 * @return a {@link Component} that is read from the XML representation and
+	 *         bound to the given {@code controller}.
 	 * 
-	 * @throws LayoutInflaterException if an error is encountered during the layout inflation.
-	 * @throws BinderException if an error is encountered during the binding.
+	 * @throws LayoutInflaterException
+	 *             if an error is encountered during the layout inflation.
+	 * @throws BinderException
+	 *             if an error is encountered during the binding.
 	 */
-	public Component create(String xmlClassResourceFileName, Object controller,
-			AttributeFilter... attributeFilters) {
-		InputStream xml = controller.getClass().getResourceAsStream(xmlClassResourceFileName);
-		return create(xml, controller, attributeFilters);
+	public Component create(String xmlClassResourceFileName, Object controller) {
+		InputStream xml = controller.getClass().getResourceAsStream(
+				xmlClassResourceFileName);
+		return create(xml, controller);
 	}
 
 	/**
-	 * Returns a {@link Component} that is read from the XML representation given as
-	 * {@link InputStream} and binds the resulting {@link Component} to the given {@code controller}
-	 * object.
+	 * Returns a {@link Component} that is read from the XML representation
+	 * given as {@link InputStream} and binds the resulting {@link Component} to
+	 * the given {@code controller} object.
 	 * 
 	 * <br>
 	 * <br>
-	 * Optionally you may also provide {@link AttributeFilter}s to do some modifications (or example
-	 * localized translations) to any attributes present in the XML representation.
+	 * Optionally you may also provide {@link AttributeFilter}s to do some
+	 * modifications (or example localized translations) to any attributes
+	 * present in the XML representation.
 	 * 
-	 * @param xml XML representation.
-	 * @param controller controller object to bind the resulting {@code Component} ( {@code null}
-	 *            allowed).
-	 * @param attributeFilters optional {@link AttributeFilter}s to do attribute modifications.
-	 * @return a {@link Component} that is read from the XML representation and bound to the given
-	 *         {@code controller}.
+	 * @param xml
+	 *            XML representation.
+	 * @param controller
+	 *            controller object to bind the resulting {@code Component} (
+	 *            {@code null} allowed).
+	 * @return a {@link Component} that is read from the XML representation and
+	 *         bound to the given {@code controller}.
 	 * 
-	 * @throws LayoutInflaterException if an error is encountered during the layout inflation.
-	 * @throws BinderException if an error is encountered during the binding.
+	 * @throws LayoutInflaterException
+	 *             if an error is encountered during the layout inflation.
+	 * @throws BinderException
+	 *             if an error is encountered during the binding.
 	 */
-	public Component create(InputStream xml, Object controller, AttributeFilter... attributeFilters) {
+	public Component create(InputStream xml, Object controller) {
+		Map<String, AttributeFilter> attributeFilterBeans = applicationContext
+				.getBeansOfType(AttributeFilter.class);
+		AttributeFilter[] attributeFilters = attributeFilterBeans.values()
+				.toArray(new AttributeFilter[attributeFilterBeans.size()]);
+		ComponentProvider springComponentProvider = (ComponentProvider) applicationContext
+				.getBean(SpringComponentProvider.class);
 		return Clara.create(xml, controller,
-				Collections.singletonList((ComponentProvider)new SpringComponentProvider(
-						applicationContext)), attributeFilters);
+				Collections.singletonList(springComponentProvider),
+				attributeFilters);
 	}
 }
